507c339ba2ab20e0b1518bb32cb6e747fa507afd
matheus.guerra <matheusguerra@outlook.com>
2021-11-01 13:42:48 -0300

    Executado exercício 12

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -9,9 +9,58 @@ A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 única passagem em cada uma das listas.
 """
 
+from heapq import merge
+from collections import deque
+
 def linear_merge(list1, list2):
     # +++ SUA SOLUÇÃO +++
-    return
+
+    # l = list1
+    # m = list2
+    # result = []
+    # i = j = 0
+    # total = len(l) + len(m)
+#
+    # while len(result) != total:
+    #     if len(l) == i:
+    #         result += m[j:]
+    #         break
+    #     elif len(m) == j:
+    #         result += l[i:]
+    #         break
+    #     elif l[i] < m[j]:
+    #         result.append(l[i])
+    #         i += 1
+    #     else:
+    #         result.append(m[j])
+    #         j += 1
+    # return result
+
+    # def mergeArray(arr1, arr2):
+    #     return list(merge(arr1, arr2))
+
+    # arr1 = list1
+    # arr2 = list2
+    # return(mergeArray(arr1, arr2))
+
+    #['aa', 'xx', 'zz'], ['bb', 'cc']
+    res = deque()
+    while len(list1 + list2) > 0:
+        if list1[-1:] > list2[-1:]:
+            res.appendleft(list1.pop())
+            print(res)
+            print(list1)
+        else:
+            res.appendleft(list2.pop())
+            print(res)
+            print(list2)
+    return list(res)
+
+
+
+    # lista_combinada = list1 + list2
+    # lista_combinada.sort()
+    # return lista_combinada
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
